e37972e7aeea89b96cde4f211f61b12952f259ed
Raul C. Rivero <rahulrcr98@gmail.com>
2022-07-17 22:17:18 -0400

    first commit

---
+++ matches.js
@@ -0,0 +1,48 @@
+async function getMatches() {
+    let url = 'https://dota-pro-upcoming-matches-api.netlify.app/.netlify/functions/index';
+    let matches = await fetch(url, {
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        }
+    }).then(response => response.json());
+    return matches.data.matches;
+}
+
+async function matchesTemplate() {
+    let matches = await getMatches();
+    let matchesTemplate = matches.map(match => {
+        if(match.first === match.second && match.second === 'TBD') 
+            return;
+        return `
+            <div class="w-full rounded flex justify-between gap-5 p-5 m-4" style="width: 370px; background: ${match.status.indexOf(':') !== -1 ? '#00afb9' : '#0081a7'}">
+                <div class="w-1/3 text-center text-white text-base">${match.first}</div>
+                <div class="w-1/3 text-center text-white font-md">${match.status}</div>
+                <div class="w-1/3 text-center text-white text-base">${match.second}</div>
+            </div>
+        `
+    }).join('');
+    return matchesTemplate;
+}
+
+function setLoadingTemplate() {
+    document.querySelector('#matches').innerHTML = `
+        <div class="w-100 flex justify-center items-center" style="min-height: 600px;">
+            <div class="lds-roller">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            </div>
+        </div>
+    `
+}
+
+setLoadingTemplate()
+
+matchesTemplate().then(matches => {
+    document.querySelector('#matches').innerHTML = matches;
+});
